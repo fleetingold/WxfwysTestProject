d58fcd639403319ef51cfea3380b9084e6a0d3ee
fleetingold <1161851523@qq.com>
2021-06-07 15:29:09 +0800

    update RedisService & RedisServiceImpl

--- src/main/java/com/wxfwys/service/RedisService.java
+++ src/main/java/com/wxfwys/service/RedisService.java
@@ -1,4 +1,54 @@
 package com.wxfwys.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public interface RedisService {
+    void ping();
+
+    void select(int index);
+
+    boolean exists(String key);
+
+    String type(String key);
+
+    void del(String key);
+
+    void rename(String oldKey, String newKey);
+
+    void renamenx(String oldKey, String newKey);
+
+    void move(String key, int dbIndex);
+
+    byte[] dump(String key);
+
+    Set<String> keys(String pattern);
+
+    void set(String key, String value);
+
+    String get(String key);
+
+    void lpush(String key, String member);
+
+    List<String> lrange(String key, long start, long stop);
+
+    void hmset(String key, String field, String value);
+
+    void hmset(String key, Map<String, String> hash);
+
+    String hget(String key, String field);
+
+    Map<String, String> hgetAll(String key);
+
+    void sadd(String key, String member);
+
+    Set<String> smembers(String key);
+
+    void zadd(String key, double score, String member);
+
+    Set<String> zrange(String key, long start, long stop);
+
+    Set<String> zrangeByScore(String key, double min, double max);
 }
--- src/main/java/com/wxfwys/service/impl/RedisServiceImpl.java
+++ src/main/java/com/wxfwys/service/impl/RedisServiceImpl.java
@@ -1,13 +1,137 @@
 package com.wxfwys.service.impl;
 
-import redis.clients.jedis.DefaultJedisClientConfig;
+import com.wxfwys.service.RedisService;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisClientConfig;
 
-public class RedisServiceImpl {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class RedisServiceImpl implements RedisService {
     private Jedis jedis;
 
     public RedisServiceImpl() {
         this.jedis = new Jedis("106.15.89.240", 6379);
+        this.jedis.auth("abc-123");
+    }
+
+    @Override
+    public void ping() {
+        if (jedis != null) {
+            System.out.println("服务正在运行: " + jedis.ping());
+        }
+    }
+
+    @Override
+    public void select(int index) {
+        jedis.select(index);
+    }
+
+    @Override
+    public boolean exists(String key) {
+        return jedis.exists(key);
+    }
+
+    @Override
+    public String type(String key) {
+        return jedis.type(key);
+    }
+
+    @Override
+    public void del(String key) {
+        jedis.del(key);
+    }
+
+    @Override
+    public void rename(String oldKey, String newKey) {
+        jedis.rename(oldKey, newKey);
+    }
+
+    @Override
+    public void renamenx(String oldKey, String newKey) {
+        jedis.renamenx(oldKey, newKey);
+    }
+
+    @Override
+    public void move(String key, int dbIndex) {
+        jedis.move(key, dbIndex);
+    }
+
+    @Override
+    public byte[] dump(String key) {
+        return jedis.dump(key);
+    }
+
+    @Override
+    public Set<String> keys(String pattern) {
+        return jedis.keys(pattern);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        jedis.set(key, value);
+    }
+
+    @Override
+    public String get(String key) {
+        return jedis.get(key);
+    }
+
+    @Override
+    public void lpush(String key, String member) {
+        jedis.lpush(key, member);
+    }
+
+    @Override
+    public List<String> lrange(String key, long start, long stop) {
+        return jedis.lrange(key, start, stop);
+    }
+
+    @Override
+    public void hmset(String key, String field, String value) {
+        Map<String, String> hash = new HashMap<>();
+        hash.put(field, value);
+        hmset(key, hash);
+    }
+
+    @Override
+    public void hmset(String key, Map<String, String> hash) {
+        jedis.hmset(key, hash);
+    }
+
+    @Override
+    public String hget(String key, String field) {
+        return jedis.hget(key, field);
+    }
+
+    @Override
+    public Map<String, String> hgetAll(String key) {
+        return jedis.hgetAll(key);
+    }
+
+    @Override
+    public void sadd(String key, String member) {
+        jedis.sadd(key, member);
+    }
+
+    @Override
+    public Set<String> smembers(String key) {
+        return jedis.smembers(key);
+    }
+
+    @Override
+    public void zadd(String key, double score, String member) {
+        jedis.zadd(key, score, member);
+    }
+
+    @Override
+    public Set<String> zrange(String key, long start, long stop) {
+        return jedis.zrange(key, start, stop);
+    }
+
+    @Override
+    public Set<String> zrangeByScore(String key, double min, double max) {
+        return jedis.zrangeByScore(key, min, max);
     }
 }
